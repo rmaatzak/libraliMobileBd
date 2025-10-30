bec9b5e58d290df545231fcc1cdc2ec87ae9c38f
rmaatzak <159541913+rmaatzak@users.noreply.github.com>
2025-10-30 09:36:53 -0300

    .
    .

---
+++ backend/server.js
@@ -0,0 +1,7 @@
+const db = mysql.createConnection({
+  host: "sql103.infinityfree.com",  // ou o host que sua hospedagem mostrar
+  user: "if0_40237895",       // seu usuário do banco
+  password: "WmQwcGmQLY",     // sua senha do banco
+  database: " if0_40237895_librali", // nome do seu banco
+  port: 3306                     // geralmente 3306, pode confirmar no painel
+});
